79e8d226304fd7bd909fb73fba402e83bc68b09b
Kaelumbs25 <s00267367@atu.ie>
2025-12-17 19:39:17 +0000

    changed js to generate products as i changed the json layout and added some design for the product cards

--- public/customJS/shop.js
+++ public/customJS/shop.js
@@ -7,7 +7,7 @@ async function fetchProducts() {
     const products = await response.json();
 
     // Call the rendering function to create HTML elements
-    renderProducts(products.book);
+    renderProducts(products);
 
   } catch (error) {
     console.error("Failed to fetch products data:", error);
@@ -20,33 +20,43 @@ function renderProducts(products) {
   const container = document.getElementById("products-container");
   container.innerHTML = ""; // Clear previous content if needed
 
-  products.forEach(book => {
+  for (const key in products) {
+    const book = products[key][0];
 
     const {
-      name, 
+      name,
       img,
       author,
       release,
       format,
-      price
+      price,
+      id
     } = book;
 
+    console.log(book)
+
+
     const productCard = document.createElement("div");
-    //productCard.classList.add("product-card");
+    productCard.setAttribute("class", "col-12 col-md-6 col-lg-4 productCard");
+    
 
     productCard.innerHTML = `
-      <img style="width: 90%; " src="${img}" alt="Image of ${name}" />
+      <a class="text-decoration-none d-flex " href="$book${id}">
+      <img class="w-50 h-50 " src="${img}" alt="Image of ${name}" />
+      <div>
       <h3>${name}</h3>
       <p>${author}</p>
       <p>${release}</p>
       <p>${format}</p>
       <p>${price}</p>
+      </div
+      </a>
     `;
 
     // Add this card to the page
     container.appendChild(productCard);
-    productCard.setAttribute("class", "col-12 col-md-6 col-lg-4");
-  });
+    productCard.classList.add("gy-4");
+  };
 }
 
 
