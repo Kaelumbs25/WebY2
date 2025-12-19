6cbe32e0776fd76eb492b58684f5560af0cd3231
Kaelumbs25 <s00267367@atu.ie>
2025-12-19 15:09:37 +0000

    removed header as there wasnt much need for it with the site, fixed nav icon widths to shrink instead of overflwinf the nav and added logo

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.get('/', (req, res) => {
   // set active for navigation
   state={home:true}
   // set specifics for <head>
-  head={title: "Home - Week 1"}
+  head={'title': "Books&Beyond", 'description': "The home page of Books&Beyond", 'keywords':"Bestsellers, Memberships, Browse"}
   // pass object to to render in "index"
   res.render('index', {state, head});
   // send this to terminal where node app is running
@@ -42,22 +42,25 @@ app.get('/', (req, res) => {
 // about route
 app.get('/about', (req, res) => {
     state={about : true}
-    head={title:"About - Week 1"}
+    head={'title': "About Us", 'description': "About our business and where we are located", 'keywords':"About Us, Story, Our Shop, Journey, Contact Us"}
     res.render('about', { state, head});
     console.log('about')
   });
 
+  //form submission route
 app.get('/submission', (req, res) => {
     userComment=req.query.userComment
     userComment=userComment.replace(/\n/g, "<br/>")
     formDetails = {userEmail:req.query.userEmail, userComment:userComment}
+    head={'title': "Conact Form Submitted", 'description': "Your contact form has been submitted", 'keywords':"comment, email"}
     res.render("submission", {formDetails});
     console.log('submission')
   });
 
+  //membership route
 app.get('/membership', (req, res) => {
     state={membership : true}
-    head={title:"Membership"}
+    head={'title': "Our Membership", 'description': "The memberhsips Books&Beyond has to offer", 'keywords':"Subscriptions, Fantasy, Sci-Fi"}
     res.render('membership', { state, head});
     console.log('membership')
   });
@@ -65,7 +68,7 @@ app.get('/membership', (req, res) => {
   // checkout route
 app.get('/checkout', (req, res) => {
     state={checkout : true}
-    head={title:"Checkout"}
+    head={'title': "Checkout", 'description': "Checkout for your order", 'keywords':"Place Order, Card Details, User"}
     res.render('checkout', { state, head});
     console.log('checkout')
   });
@@ -73,7 +76,7 @@ app.get('/checkout', (req, res) => {
   // login route
 app.get('/login', (req, res) => {
     state={login : true}
-    head={title:"Login"}
+    head={'title': "Login", 'description': "Books&Beyond Login", 'keywords':"Log in, Password, Username"}
     res.render('login', { state, head});
     console.log('login')
   });
@@ -81,7 +84,7 @@ app.get('/login', (req, res) => {
   // shop route
 app.get('/shop', (req, res) => {
     state={shop : true}
-    head={title:"Shop"}
+    head={'title': "Books&Beyond Shop", 'description': "Our plethora of books for your enjoyment", 'keywords':"hardback, Released, By"}
     res.render('shop', { state, head});
     console.log('shop')
   });
@@ -89,7 +92,7 @@ app.get('/shop', (req, res) => {
   // user details route
 app.get('/userDetails', (req, res) => {
     state={userDetails : true}
-    head={title:"User Details"}
+    head={'title': "Your Details", 'description': "Details for the users account", 'keywords':"Address, E-mail, Phone, Details"}
     res.render('userDetails', { state, head});
     console.log('userDetails')
   });
@@ -97,21 +100,22 @@ app.get('/userDetails', (req, res) => {
   // cart route
 app.get('/cart', (req, res) => {
     state={cart : true}
-    head={title:"Shopping Cart"}
+    head={'title': "Your Book Haul", 'description': "The home page of The Games Hub", 'keywords':"Trailers, Reviews, community, Benefits, Events"}
     res.render('cart', { state, head});
     console.log('cart')
   });
 
+  //success route
 app.get('/success', (req, res) => {
     state={cart : true}
-    head={title:"Order Success"}
+    head={'title': "Order Successful", 'description': "Confirmation that your order was placed", 'keywords':"Successfully, Order"}
     res.render('success', { state, head});
     console.log('success')
   });
 
   //product adaptive route
 app.get('/product/:id', (req,res) => {
-  head={title:"Product "+req.params.id}
+  head={'title': "Product "+req.params.id, 'description': "TProduct Details Page", 'keywords':"Hardback, Paperback, Add to Cart"}
   res.render('product', {id: req.params.id})
   console.log('product'+req.params.id)
 });
